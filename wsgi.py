192b95240eef25123122690588aed822b851c1cf
40223151 <40223151@gm.nfu.edu.tw>
2015-05-26 01:30:43 +0800

    md menuLink1

--- wsgi.py
+++ wsgi.py
@@ -85,12 +85,18 @@ class Hello(object):
     #@+node:2014fall.20141215194146.1791: *3* index
     @cherrypy.expose
     def index(self):
-        outstring='''
+        outstring=''' 
     <html>
     <font size='6' color='darkslateblue' face='標楷體' >
     協同產品設計分組報告
-    <br />
+    </font><br />
+     <font size='4' color='darkslateblue' face='標楷體' >
     第八組 40223151網站
+    <br />
+    </html>'''
+        outstring +='''
+    <html>
+    <br />
     <br />
     <table border=3>
 　 <tr>
@@ -130,6 +136,11 @@ class Hello(object):
     def index1(self):
         outstring ='''<font size='6' color='darkslateblue' face='標楷體' >個人影片</font>'''
         outstring += "</br>"
+        outstring += self.menuLink1()
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
         outstring += "學號:40223151"
         outstring += "<br />"
         outstring += "姓名:簡正斌"
@@ -156,7 +167,12 @@ class Hello(object):
         cherrypy.session['answer'] = theanswer
         cherrypy.session['count'] = thecount
         # 印出讓使用者輸入的超文件表單
-        outstring = '''
+        outstring = self.menuLink1()
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += '''
     <!DOCTYPE html> 
     <html>
     <head>
@@ -199,7 +215,12 @@ class Hello(object):
         cherrypy.session['answer'] = theanswer
         cherrypy.session['count'] = thecount
         # 印出讓使用者輸入的超文件表單
-        outstring = '''
+        outstring = self.menuLink1()
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += '''
     <!DOCTYPE html> 
     <html>
     <head>
@@ -389,7 +410,12 @@ gear(400,400,300,41,"blue")
         cherrypy.session['answer'] = theanswer
         cherrypy.session['count'] = thecount
         # 印出讓使用者輸入的超文件表單
-        outstring = '''
+        outstring = self.menuLink1()
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += "</br>"
+        outstring += '''
     <!DOCTYPE html> 
     <html>
     <head>
@@ -973,6 +999,147 @@ gear(400,400,'''+str(K)+''','''+str(N)+''',"blue")
         <a href="drawspur">2015cda 期中上機考 一顆齒輪繪圖</a>|
         <a href="drawspur1"> 2015cda 期中上機考表單文字輸出</a>|
         <a href="index1"> 個人影片</a>|<br />
+    
+        '''
+    def menuLink1(self):
+        return '''
+        <html>
+            <head>
+                <meta charset="UTF-8" />
+                <title>純 CSS 選單 Example</title>
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+                <script type="text/javascript">
+                    $(function(){
+                        $("ul.navigation > li:has(ul) > a").append('<div class="arrow-bottom"></div>');
+                        $("ul.navigation > li ul li:has(ul) > a").append('<div class="arrow-right"></div>');
+                    });
+                </script>
+                <style type="text/css">
+
+                    /*
+                        作者：Yuxin
+                        教學文：http://fundesigner.net/only-css-menu
+                        授權：MIT License
+                    */
+
+                    /* 初始化 */
+                    body, ul, li, a{
+                        margin: 0;
+                        padding: 0;
+                        font-size: 13px;
+                        text-decoration: none;
+                    }
+                    ul, li {
+                        list-style: none;
+                    }
+                    /* 選單 li 之樣式 */
+                    ul.navigation li {
+                        position: relative;
+                        float: left;
+                    }
+                    /* 選單 li 裡面連結之樣式 */
+                    ul.navigation li a{
+                        display: block;
+                        padding: 12px 20px;
+                        background: #888;
+                        color: #FFF;
+                    }
+                    /* 特定在第一層，以左邊灰線分隔 */
+                    ul.navigation > li > a{
+                        border-bottom: 1px solid #CCC;				
+                        border-left: 1px solid #CCC;
+                    }
+                    ul.navigation > li > a:hover{
+                        color: #666;
+                        background: #DDD
+                    }
+                    /* 特定在第一層 > 第二層或以後下拉部分之樣式 */
+                    ul.navigation ul{
+                        display: none;
+                        float: left;
+                        position: absolute;			
+                        left: 0;	
+                        margin: 0;
+                    }
+                    /* 當第一層選單被觸發時，指定第二層顯示 */
+                    ul.navigation li:hover > ul{
+                        display: block;
+                    }			
+                    /* 特定在第二層或以後下拉部分 li 之樣式 */
+                    ul.navigation ul li {
+                        border-bottom: 1px solid #DDD;
+                    }
+                    /* 特定在第二層或以後下拉部分 li （最後一項不要底線）之樣式 */
+                    ul.navigation ul li:last-child {
+                        border-bottom: none;
+                    }
+                    /* 第二層或以後選單 li 之樣式 */
+                    ul.navigation ul a {
+                        width: 120px;
+                        padding: 10px 12px;	
+                        color: #666;		
+                        background: #EEE;			
+                    }
+                    ul.navigation ul a:hover {		
+                        background: #CCC;				
+                    }
+                    /* 第三層之後，上一層的選單觸發則顯示出來（皆為橫向拓展） */
+                    ul.navigation ul li:hover > ul{
+                        display: block;
+                        position: absolute;
+                        top: 0;				
+                        left: 100%;
+                    }
+                    /* 箭頭向下 */
+                    .arrow-bottom {
+                        display: inline-block;
+                        margin-left: 5px;
+                        border-top: 4px solid #FFF;
+                        border-right: 4px solid transparent;				
+                        border-left: 4px solid transparent;		
+                        width: 1px;
+                        height: 1px;
+                    }
+
+                    /* 箭頭向右 */
+                    .arrow-right {
+                        display: inline-block;
+                        margin-left: 12px;	
+                        border-top: 3px solid transparent;
+                        border-bottom: 3px solid transparent;
+                        border-left: 3px solid #666;		
+                        width: 1px;
+                        height: 1px;
+                    }
+                </style>		
+            </head>
+            <body>
+                <ul class="navigation">
+                    <li><a href=\"index\">首頁</a><br /></li>
+                    <li>
+                        <a href="#">2015cda-w11</a>
+                        <ul>
+                            <li><a href="spur">七顆齒輪</a></li>
+                            <li>
+                                <a href="#">心得</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="i#">2015cda-w12</a><br /></li>
+                    <li><a href="#">2015cda-w13</a><br /></li>
+                    <li><a href="#">2015cda-w14</a><br /></li>
+                    <li><a href="index1"> 個人影片</a><br /></li>
+                    <li>
+                        <a href="#">2015cda 期中上機考</a>
+                        <ul>
+                            <li><a href="drawspur">2015cda 期中上機考 一顆齒輪繪圖</a></li>
+                            <li><a href="drawspur1"> 2015cda 期中上機考表單文字輸出</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="gear">3D齒輪模式</a></li>
+                </ul>
+            </body>
+        </html>
         '''
 #@-others
 ################# (4) 程式啟動區
